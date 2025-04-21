cc6165631832647e48b16cdcba631194315f9ffa
clintbelew <clintbelew@hotmail.com>
2025-04-21 15:48:18 -0500

    Delete config directory

--- config/config.py
+++
@@ -1,7 +0,0 @@
-import os
-
-# ElevenLabs API key
-ELEVENLABS_API_KEY = os.environ.get('ELEVENLABS_API_KEY', 'your_elevenlabs_api_key_here')
-
-# Deployment base URL
-BASE_URL = os.environ.get('BASE_URL', 'https://vanguard-voice-bot.onrender.com')
